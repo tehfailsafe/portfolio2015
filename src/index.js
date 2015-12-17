b9f4a42c3d22f99dd3a4024325853aef1daaa795
Mike Johnson <mike@failsafedesign.com>
2015-12-16 16:54:19 -0800

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createHistory, useBasename } from 'history'
-import CreateBrowserHistory from 'history/lib/createBrowserHistory'
 import { Router, Route } from 'react-router'
 
 import App from "./App";
 import Test from "./Test2";
 
 
-const history = CreateBrowserHistory();
+const history = useBasename(createHistory)({
+  basename: '/portfolio2015'
+})
 
 render(
   <Router history={history}>

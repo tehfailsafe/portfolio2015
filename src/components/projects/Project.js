d61ac6f8f0a6f8225afeae960c93e244471cb55f
Mike Johnson <mike@failsafedesign.com>
2015-12-31 10:31:14 -0800

    update

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -10,7 +10,8 @@ class Project extends React.Component{
     this.state = {
       open: false,
       containerStyle: {},
-      thumbnailStyle: {}
+      thumbnailStyle: {},
+      backgroundStyle: {}
     }
   }
 
@@ -20,13 +21,19 @@ class Project extends React.Component{
       this.props.pckry.layout();
       this.pos = this.getPos(this.refs.thumbnail);
       this.setState({
-        containerStyle: {height: this.pos.h}
+        containerStyle: {height: this.pos.h},
+        backgroundStyle: {height: this.pos.h, width: this.pos.w}
       })
+      console.log(this.refs);
+      TweenMax.set(this.refs.project, {perspective:800});
+      TweenMax.set(this.refs.thumbnailContainer, {transformStyle: "preserve-3d"})
+      TweenMax.set(this.refs.background, {rotationY:-180})
+      TweenMax.set([this.refs.background, this.refs.thumbnail], {backfaceVisibility: "hidden"})
     });
   }
 
   handleResize(){
-    this.pos = this.getPos(this.refs.thumbnail);
+    this.pos = this.getPos(this.refs.thumbnailContainer);
     this.setState({
       containerStyle: {height: this.pos.h}
     })
@@ -34,21 +41,21 @@ class Project extends React.Component{
   }
 
   onClick(e){
-    var container = this.refs.project;
-    var circle = this.refs.circle;
-    var thumbnail = this.refs.thumbnail;
-
     if(!this.state.open){
-      this.pos = this.getPos(container);
-      console.log(this.pos);
+      this.pos = this.getPos(this.refs.project);
+
       this.setState({
         containerStyle: { zIndex: 100, top: 0, left: 0, width: "100vw", height: "100vh", position: "fixed" },
         thumbnailStyle:{ width: this.pos.w, top: this.pos.y, left: this.pos.x, position: "absolute"},
+        backgroundStyle: {width: "100%", height: "100%"},
         open: true
       })
-      TweenMax.fromTo(circle, 1, {top: this.pos.y + this.pos.h / 2, left: this.pos.x + this.pos.w/2},  { scale: 50, ease: Quad.easeOut });
+
+      // TweenMax.fromTo(this.refs.circle, 1.8, {top: this.pos.y + this.pos.h - 100, left: this.pos.x + this.pos.w - 100, scale: 1},  { scale: 25, ease: Power2.easeOut });
+      TweenMax.to(this.refs.thumbnailContainer, .75, { width: "80%", left: "10%", top: "10%", rotationY: 180, ease: Power2.easeInOut})
     } else {
-      TweenMax.to(circle, .5, {scale: 1, ease: Expo.easeOut, onComplete:()=> {
+      TweenMax.to(this.refs.thumbnailContainer, .5, { rotationY: 0, width: this.pos.w, top: this.pos.y, left: this.pos.x, ease: Power2.easeOut})
+      TweenMax.to(this.refs.circle, .5, {scale: 0, ease: Power2.easeOut, onComplete:()=> {
         this.setState({
           containerStyle: { zIndex: this.pos.z, top: this.pos.y - this.getPos(this.props.parent).y, left: this.pos.x, height: this.pos.h, position: "absolute" },
           thumbnailStyle: {top: 0, left: 0},
@@ -62,7 +69,10 @@ class Project extends React.Component{
     var thumbnailClass = this.state.open ? "thumbnail" : "thumbnail img-fluid"
     return(
       <div ref="project" className="item col-sm-4 col-xs-12 projectContainer" style={this.state.containerStyle} onClick={this.onClick.bind(this)}>
-        <img ref="thumbnail" className={thumbnailClass} style={this.state.thumbnailStyle} src={this.props.project.tn} />
+        <div ref="thumbnailContainer" className="thumbnailContainer" style={this.state.thumbnailStyle} >
+          <div ref="background" style={this.state.backgroundStyle} className="white-bg"></div>
+          <img ref="thumbnail" className="thumbnail img-fluid"  src={this.props.project.tn} />
+        </div>
         <div ref="circle" className="circle"/>
       </div>
     )

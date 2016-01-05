3a273d5c8088bb6e0c330424576855b202391465
Mike Johnson <mike@failsafedesign.com>
2016-01-04 20:40:04 -0800

    Basename

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ require('./assets/styles/main.scss');
 
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-// import { createHistory, useBasename } from "history"
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router'
+import { createHistory, useBasename } from "history"
 
 import App from "./App";
 import About from "./components/About";
@@ -12,10 +12,10 @@ import Projects from "./components/projects/Projects";
 import Project from "./components/projects/Project";
 
 
-// const history = useRouterHistory(createHistory)({ basename: "/portfolio2015" })
+const history = useRouterHistory(createHistory)({ basename: "/portfolio2015" })
 
 render(
-  <Router history={browserHistory}>
+  <Router history={history}>
     <Route component={App}>
       <Route path="/" component={Projects}>
         <Route path="projects/:projectId" component={Project}/>
